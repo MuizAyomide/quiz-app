ae33c9e9aeb8e26ca36d57de62201bc2e7c7f003
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-03-26 02:36:52 +0100

    initialization

--- script.js
+++ script.js
@@ -56,6 +56,7 @@ function startQuiz(){
 }
 
 function showQuestion(){
+    resetState();
     let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
     questionElement.innerHTML = questionNo + '. ' + currentQuestion.question;
@@ -65,5 +66,31 @@ function showQuestion(){
         button.innerHTML = answer.text;
         button.classList.add('btn');
         answerButton.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener('click', selectAnswer)
     })
 }
+
+
+function resetState(){
+    nextButton.style.display = 'none';
+    while(answerButton.firstChild){
+        answerButton.removeChild(answerButton.firstChild);
+    }
+}
+
+
+function selectAnswer(e){
+    const selectBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === 'true';
+    if(isCorrect){
+        selectBtn.classList.add('correct');
+    }
+    else{
+        selectBtn.classList.add('incorrect');
+    }
+    }
+
+startQuiz();
